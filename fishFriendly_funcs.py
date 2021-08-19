5733cbc80338980a5d881f0540419d5523a8983e
Alix Macdonald <alixmacdonald10@googlemail.com>
2021-08-19 16:32:39 +0100

    WIP implementation with NUMBA some errors remaining. Currently around v strike

--- fishFriendly_funcs.py
+++ fishFriendly_funcs.py
@@ -53,43 +53,42 @@ def find_nearest(array, value):
     val = array[idx]
     return idx, val
 
-@jit
-def NEN_analyse(pump_db, fish_db, intake, n_steps=30):
-    fish_type = fish_db['fish_type']
+@njit
+def NEN_analyse(Q, H, N, D_blade, d_blade, n_blade, NEN_r_array, NEN_beta_array, NEN_delta_array, imp_thk, r_imp_thk, L_f, B_f, fish_type, intake, n_steps=30): 
     # calculate effective length of fish
-    L_eff = length_eff(fish_db['L_f'], fish_db['B_f'], fish_db['fish_type'], intake)
+    L_eff = length_eff(L_f, B_f, fish_type, intake)
     # define max length of  head and flow curve series
-    Q_points = len(pump_db['Q'])
-    H_points = len(pump_db['H'])
+    Q_points = len(Q)
+    H_points = len(H)
     # change in radius per step
-    R_o = pump_db['D_blade'] / 2
-    R_i = pump_db['d_blade'] / 2
+    R_o = D_blade / 2
+    R_i = d_blade / 2
     d_r = (R_o - R_i) / n_steps
     #Initialise zero matrices as correct size for storing variables at the end of the loop
-    v_strike_max_array= np.zeros([Q_points, H_points], dtype=np.float64, order='C')
-    P_th = np.zeros([Q_points, H_points], dtype=np.float64, order='C')
-    f_MR = np.zeros([Q_points, H_points], dtype=np.float64, order='C')
-    P_m = np.zeros([Q_points, H_points], dtype=np.float64, order='C')
+    v_strike_max_array= np.zeros((H_points, Q_points), dtype=np.float32)
+    P_th = np.zeros((H_points, Q_points), dtype=np.float32)
+    f_MR = np.zeros((H_points, Q_points), dtype=np.float32)
+    P_m = np.zeros((H_points, Q_points), dtype=np.float32)
     # for each radial position along the blade and for each flowrate the
     # mortality rate will be calculated.
     for i in range(0, Q_points):
         # flow
-        Q_temp = pump_db['Q'][i]
+        Q_temp = Q[i]
         # flow velocity
         area = np.pi * (R_o**2 - R_i**2)
         v_m = Q_temp / area
         # loop through head values
         for j in range(0, H_points):
             # head
-            H_temp = pump_db['H'][j]
+            H_temp = H[j]
             # determine pump speed required for head and flow value
-            N_pump_scale = N_scale(pump_db['N'], pump_db['H'][i], H_temp)
+            N_pump_scale = N_scale(N, H, H_temp)
             # determine rotational speed
             omega = (2.0 * np.pi * N_pump_scale) / 60.0  # rad/s
             # initial area - reset at each duty position
-            A = 0
+            A = 0.0
             # step along blade at this duty and sum factors
-            v_strike_max = 0
+            v_strike_max = 0.0
             for i_r in range(0, n_steps):
                 # radial position
                 r = R_i + (i_r) * d_r + (d_r / 2)  # removed - 1 due to index
@@ -98,21 +97,22 @@ def NEN_analyse(pump_db, fish_db, intake, n_steps=30):
                 # new area - overwriting previous A value
                 A = A + d_A
                 # collision probabilty
-                dP_th = collision_probability(L_eff, v_m, omega, r, pump_db['n_blade'])
+                dP_th = collision_probability(L_eff, v_m, omega, r, n_blade)
                 # determine angles at radial position
                 r_angle_lookup = r / R_o
-                r_angle_idx, _ = find_nearest(pd.Series(pump_db['NEN_r_array']), r_angle_lookup)
-                beta_deg = pump_db['NEN_beta_array'][r_angle_idx]
+                r_angle_idx, _ = find_nearest(NEN_r_array, r_angle_lookup)
+                beta_deg = NEN_beta_array[r_angle_idx]
                 beta = np.radians(beta_deg)
-                delta_deg = pump_db['NEN_delta_array'][r_angle_idx]
+                delta_deg = NEN_delta_array[r_angle_idx]
                 delta = np.radians(delta_deg)
                 # strike velocity
+                print(v_m, omega, r, beta, delta)
                 v_strike = strike_velocity(v_m, omega, r, beta, delta)
                 if v_strike > v_strike_max:
                     v_strike_max = v_strike
                 # determine thickness at radial position
-                r_thk_idx, _ = find_nearest(pd.Series(pump_db['r_imp_thk']), r * 1e3)
-                d = pump_db['imp_thk'][r_thk_idx]
+                r_thk_idx, _ = find_nearest(r_imp_thk, r * 1e3)
+                d = imp_thk[r_thk_idx]
                 # mortality factor
                 df_MR = mortality_factor(L_eff, d, v_strike, fish_type)
                 # mortality probabilty
@@ -128,37 +128,8 @@ def NEN_analyse(pump_db, fish_db, intake, n_steps=30):
             P_m[j][i] = P_m[j][i] / Q_temp
 
     return v_strike_max_array, P_th, f_MR, P_m
-
-
-def NEN_analyse_VECT(pump_db, fish_db, intake, n_steps=30):
-    # calculate effective length of fish
-    L_eff = length_eff(fish_db['L_f'], fish_db['B_f'], fish_db['fish_type'], intake)
-    # calc impeller details
-    R_o = pump_db['D_blade'] / 2
-    R_i = pump_db['d_blade'] / 2
-    d_r = (R_o - R_i) / n_steps
-    
-    # create database over correct size
-    df = pd.DataFrame(data=pump_db, columns=['Q', 'H'])
-    print(df.head())
-    df_full = pd.concat([df] * n_steps)
-    print(len(df))
-    # radial position
-    r_lst = []
-    for i_r in range(0, n_steps - 1):
-        r = R_i + (i_r) * d_r + (d_r / 2)
-        r_lst.append(r)
-        # determine angles at each position
-        r_angle_idx, _ = find_nearest(pd.Series(pump_db['NEN_r_array']), r / R_o)
-        beta_deg = pump_db['NEN_beta_array'][r_angle_idx]
-        beta = np.radians(beta_deg)
-        delta_deg = pump_db['NEN_delta_array'][r_angle_idx]
-        delta = np.radians(delta_deg)
-        r_thk_idx, _ = find_nearest(pd.Series(pump_db['r_imp_thk']), r * 1e3)
-        d = pump_db['imp_thk'][r_thk_idx]
     
     
-
 @njit
 def length_eff(L_f, B_f, fish_type, intake):
     
@@ -177,15 +148,17 @@ def length_eff(L_f, B_f, fish_type, intake):
             # L_eff = max(L_eff_1, L_eff_2, L_eff_3, L_eff_4)
     return L_eff
 
-@njit  # THIS SLOWS DOWN FROM 50.803 TO 56.797!
+
+@njit 
 def collision_probability(L_eff, v_m, omega, r, n_blade, wf=0, alpha=0):
 
     # collision probability - assume no fish relative motion and no preswirl
     P_th = (L_eff * n_blade * omega * r) / (v_m  * 2 * np.pi * r)
 
     # probability can only be from 0 - 1 therefore:
-    P_th = max(0, min(1, P_th))
+    P_th = np.maximum(0.0, np.minimum(1.0, P_th))
     return P_th
+
 
 @njit
 def strike_velocity(v_m, omega, r, beta, delta):
@@ -194,6 +167,7 @@ def strike_velocity(v_m, omega, r, beta, delta):
         (v_m * np.cos(beta))**2 + ((omega * r) * np.cos(delta))**2
     )
     return v_strike
+
 
 @njit
 def mortality_factor(L_f, t_blade, v_strike, fish_type):
@@ -225,29 +199,29 @@ def mortality_factor(L_f, t_blade, v_strike, fish_type):
             #warnings.warn(f'Lf/d ratio {ratio} exceeds model limits (25)')
             # place holder if data is revisted
             a = 0.0024
-            b = 0
-            v_crit = 8
+            b = 0.0
+            v_crit = 8.0
         elif ratio < 1:
             #warnings.warn(f'Lf/d ratio {ratio} exceeds model lower limit (1)')
             # place holder if data is revisted
             a = 0.0024
-            b = 0
-            v_crit = 8
+            b = 0.0
+            v_crit = 8.0
         else:
             a = 0.0024
-            b = 0
-            v_crit = 8
+            b = 0.0
+            v_crit = 8.0
 
     # determine mortality factor
     if v_strike < v_crit:
-        f_MR = 0
+        f_MR = 0.0
     else:
         if fish == "fish":
             f_MR = (a * np.log(ratio) + b) * (v_strike - v_crit)
         else:
             f_MR = (a * ratio) * (v_strike - v_crit)
     # bounded by probabilty between 0 and 1
-    f_MR = max(0, min(1, f_MR))
+    f_MR = np.maximum(0.0, np.minimum(1.0, f_MR))
     return f_MR
 
 
@@ -274,13 +248,21 @@ def plot_result(pump_db, result, duty_db, title):
     ax.annotate(f"{pump_db['N']} RPM", (pump_db['Q'][8], pump_db['H'][8]))
     # plot duty point or BEP
     plt.scatter(duty_db['Q'], duty_db['H'])
-    Q_idx, _ = find_nearest(pump_db['Q'], duty_db['Q'])
-    H_idx, _ = find_nearest(pump_db['H'], duty_db['H'])
-    plt.text(duty_db['Q'], duty_db['H'], f'  {format(result[Q_idx, H_idx], ".2f")}')
-        
+    duty_mortality = duty_mort(pump_db, duty_db, P_m)
+    plt.text(duty_db['Q'], duty_db['H'], f'  {format(duty_mortality, ".2f")}')
     plt.show(block=False)
     
     return fig
+
+
+def duty_mort(pump_db, duty_db, P_m):
+    Q_idx, _ = find_nearest(pump_db['Q'], duty_db['Q'])
+    H_idx, _ = find_nearest(pump_db['H'], duty_db['H'])
+    duty_mortality = format(P_m[Q_idx, H_idx] * 100, ".2f")
+    
+    return duty_mortality
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
+from numpy import dtype
 import fishFriendly_funcs as main
 import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import timeit
+from numba import jit, njit
 
 
-
-def test():
-    
+def input():
     pump_des = 'CBF'
     pump_size = '140'
     if 'BF' in pump_des:
@@ -52,36 +53,56 @@ def test():
             pd.Series(pump_db['P']),
             pump_db['N']
         )
-        pump_db['Q'] = Q_scaled
-        pump_db['H'] = H_scaled
-        pump_db['effy'] = pd.Series(pump_db['effy'])
-        pump_db['P'] = P_scaled
+        pump_db['Q'] = Q_scaled.to_numpy(dtype='float32')
+        pump_db['H'] = H_scaled.to_numpy(dtype='float32')
+        pump_db['effy'] = pd.Series(pump_db['effy']).to_numpy(dtype='float32')
+        pump_db['P'] = P_scaled.to_numpy(dtype='float32')
         pump_db['N'] = N
     
+    return pump_db, fish_db, intake, Q_duty, fish_type, pump_name, pump_speed, L_f
+
+
+def run(pump_db, fish_db, intake, Q_duty):
     # analyse to standard
-    v_strike, P_th, f_MR, P_m = main.NEN_analyse(pump_db, fish_db, intake, n_steps=30)
+    L_f = fish_db['L_f']
+    B_f = fish_db['B_f']
+    fish_type = fish_db['fish_type']
+    # pump inputs 
+    Q = pump_db['Q']
+    H = pump_db['H']
+    N = pump_db['N']
+    D_blade = pump_db['D_blade']
+    d_blade = pump_db['d_blade']
+    n_blade = pump_db['n_blade']
+    NEN_r_array = np.array(pump_db['NEN_r_array'], dtype=np.float64)
+    NEN_beta_array = np.array(pump_db['NEN_beta_array'], dtype=np.float64)
+    NEN_delta_array = np.array(pump_db['NEN_delta_array'], dtype=np.float64)
+    imp_thk = np.array(pump_db['imp_thk'], dtype=np.float64)
+    r_imp_thk = np.array(pump_db['r_imp_thk'], dtype=np.float64)
+    v_strike, P_th, f_MR, P_m = main.NEN_analyse(Q, H, N, D_blade, d_blade, n_blade, NEN_r_array, NEN_beta_array, NEN_delta_array, imp_thk, r_imp_thk, L_f, B_f, fish_type, intake, n_steps=30)
     if Q_duty is not None:
         idx_duty, _ = main.find_nearest(pump_db['Q'], Q_duty)
         duty_db = {'H': pump_db['H'][idx_duty], 'Q': pump_db['Q'][idx_duty]}
     else:
         BEP_effy = float(max(pump_db['effy']))
         idx_BEP, _ = main.find_nearest(pump_db['effy'], BEP_effy)
-        duty_db = {'H': pump_db['H'][idx_BEP], 'Q': pump_db['Q'][idx_BEP]}
-        
-    Q_idx, _ = main.find_nearest(pump_db['Q'], duty_db['Q'])
-    H_idx, _ = main.find_nearest(pump_db['H'], duty_db['H'])
-    duty_mortality = format(P_m[Q_idx, H_idx] * 100, ".2f")
-    print(f'Mortality probability: {(duty_mortality)}%')
+        duty_db = {'H': pump_db['H'][idx_BEP], 'Q': pump_db['Q'][idx_BEP]}   
 
+    return P_m, duty_db
+
+
+def test():
+    
+    pump_db, fish_db, intake, Q_duty, fish_type, pump_name, pump_speed, L_f = input()
+    P_m, duty_db = run(pump_db, fish_db, intake, Q_duty)
     # plot
     title = f'NEN 8775 Mortality Probability\n\n Pump Type: {pump_name}\nPump Speed: {pump_speed} RPM\nFish Type: {fish_type}\nFish Length: {L_f} m'
     fig = main.plot_result(pump_db, P_m, duty_db, title)
-    fig.show()
-    
+    fig.show()  
 
 
 if __name__ == '__main__':
 
-    number = 5
+    number = 1
     time = timeit.timeit(test, number=number)
     print(f'Total time = {time/number} s per run')
